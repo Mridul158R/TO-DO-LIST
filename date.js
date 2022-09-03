dec5500eb6e861f16dcc0905a40bdbe59d734951
Mridul158R <mridultiwari70@gmail.com>
2022-09-03 05:32:38 +0530

    Initial commit

---
+++ date.js
@@ -0,0 +1,12 @@
+exports.getDate = function(){    
+    var options = { weekday: 'long', month: 'long', day: 'numeric' };
+    var today = new Date();
+    var day = today.toLocaleDateString("en-US", options);
+    return day;
+};
+exports.getDay = function (){    
+    var options = { weekday: 'long' };
+    var today = new Date();
+    var day = today.toLocaleDateString("en-US", options);
+    return day;
+};
